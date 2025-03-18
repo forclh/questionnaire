c3c000850a5009ff9dda7f0367e81dceac0b1dda
AK <forclh@qq.com>
2025-03-18 16:42:28 +0800

    定义全局样式

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+// 引入全局样式
+import './assets/css/index.scss'
 
 const app = createApp(App)
 
