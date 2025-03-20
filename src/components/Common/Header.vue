04938ac98d20244a15f8c20b036e12005a3fc0b5
AK <forclh@qq.com>
2025-03-20 12:07:03 +0800

    封装Header组件

---
+++ src/components/Common/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container flex align-items-center">
+    <div class="left flex justify-content-center align-items-center">
+      <el-button circle size="small" :icon="ArrowLeft" @click="goHome" />
+    </div>
+    <div class="center"></div>
+    <div class="right flex align-items-center justify-content-center">
+      <el-avatar :size="30" :src="avatar" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ArrowLeft } from '@element-plus/icons-vue';
+import { useRouter } from 'vue-router';
+import avatar from '@/assets/img/avatar.jpg';
+
+const router = useRouter();
+
+const goHome = () => {
+  router.push('/');
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100%;
+  height: 50px;
+  border-bottom: 1px solid var(--border-color);
+
+  .left {
+    width: 60px;
+    height: 100%;
+  }
+
+  .center {
+    flex: 1; // 弹性盒子存在剩余空间则拉升
+    height: 100%;
+    border-left: 1px solid var(--border-color);
+    border-right: 1px solid var(--border-color);
+  }
+
+  .right {
+    width: 80px;
+    height: 100%;
+  }
+}
+</style>
--- src/views/MaterialsView/index.vue
+++ src/views/MaterialsView/index.vue
@@ -1,7 +1,10 @@
 <template>
+  <Header />
   <div>组件市场</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from '@/components/Common/Header.vue';
+</script>
 
 <style scoped></style>
